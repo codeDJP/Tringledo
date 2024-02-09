1fe83f5f906c37c62e597d395a8cf3fd5234eee0
Dinusha Jayaranga Prabash Kumara <71733605+codeDJP@users.noreply.github.com>
2024-02-10 02:26:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/hero/Hero'
 import Services from '@/components/services/Services'
 import Why from '@/components/why/Why'
 import Technologies from '@/components/technologies/Technologies'
-import Work from '@/components/work/Work'
+// import Work from '@/components/work/Work'
 import NumOf from '@/components/numOf/NumOf'
 // import Team from '@/components/team/Team'
 
@@ -17,7 +17,7 @@ export default function Home() {
       <Why/>
       <Services/>
       <Technologies/>
-      <Work/>
+{/*       <Work/> */}
 {/*       <Team/> */}
       <NumOf/>
     </>
